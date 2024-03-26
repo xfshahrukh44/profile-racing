ac04359b590d077dea11597bc74bd1b46c18fcc3
xfshahrukh44 <ssidduit@gmail.com>
2024-03-27 01:22:58 +0500

    recycling

--- resources/views/recycling.blade.php
+++ resources/views/recycling.blade.php
@@ -27,6 +27,9 @@
 
 <section class="inner-about-sec for-inner history-pg recycling-pg">
     <div class="container">
+        <p class="mb-4" style="color: white;">
+            Profile makes all of our products in our machine shop right here in Saint Petersburg, Florida. The 16 CNC machines each generate a fair amount of “chips” (extra bits of metal) during the process of turning solid bar stock into a hub or a stem or a sprocket. We recycle all of this waste material.
+        </p>
         <div class="row">
             @foreach($recyclings as $recycling)
                 <div class="col-lg-6">
