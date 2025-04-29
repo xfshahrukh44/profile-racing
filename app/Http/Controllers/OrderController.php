0796798005c16d3bb2434c7a0ee1cd9a127a6dfd
Owais175 <owaisameer014@gmail.com>
2025-04-30 02:00:45 +0500

    new commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -327,229 +327,9 @@ class OrderController extends Controller
         return view('account.success');
     }
 
-    // public function placeOrder(Request $request)
-    // {
-
-    //     // Add these near the top of your placeOrder method
-    //     $discountAmount = 0;
-    //     $giftCardAmount = 0;
-
-    //     // Calculate discount if applied
-    //     if ($request->has('discount_code')) {
-    //         $discount = Discount::where('code', $request->discount_code)->first();
-    //         if ($discount) {
-    //             $discountAmount = $subtotal * ($discount->percentage / 100);
-    //         }
-    //     }
-
-    //     // Calculate gift card if applied
-    //     if ($request->has('gift_card_code')) {
-    //         $giftCard = GiftCard::where('code', $request->gift_card_code)->first();
-    //         if ($giftCard && $giftCard->balance > 0) {
-    //             $giftCardAmount = min($giftCard->balance, $subtotal);
-    //         }
-    //     }
-
-    //     // Then update your order total calculation:
-    //     $order->order_item_total = $subtotal;
-    //     $order->discount = $discountAmount;
-    //     $order->gift = $giftCardAmount;
-    //     $order->order_total = $subtotal + $request->shipping_amount - $discountAmount - $giftCardAmount;
-
-    //     $validateArr = array();
-    //     $messageArr = array();
-    //     $validateArr['country'] = 'required|max:50';
-    //     $validateArr['first_name'] = 'required|max:255';
-    //     $validateArr['address_line_1'] = 'required|max:255';
-    //     $validateArr['city'] = 'required|max:50';
-    //     $validateArr['email'] = 'required|max:20';
-    //     // $validateArr['phone_no'] = 'required|max:20';
-    //     $messageArr['first_name.required'] = 'The first name field is required.';
-
-    //     $id = 0;
-    //     if (isset($_POST['create_account'])) {
-
-
-    //         if ($_POST['password'] == '') {
-
-    //             $validateArr['password'] = 'min:6|required_with:confirm_password|same:confirm_password';
-    //             $validateArr['confirm_password'] = 'min:6';
-    //         } else {
-
-    //             $validateArr['email'] = 'required|max:255|email|unique:users';
-    //             $this->validate($request, $validateArr, $messageArr);
-
-    //             $pw = Hash::make($_POST['password']);
-    //             $fullName = $request->first_name . " " . $request->last_name;
-
-    //             DB::insert("INSERT INTO users(email,name,password) values('" . $_POST['email'] . "','" . $fullName . "','" . $pw . "')");
-
-
-    //             $user = DB::table('users')->orderBy('id', 'desc')->first();
-    //             $id = $user->id;
-    //         }
-    //     }
-
-    //     $validateArr['email'] = 'required|max:255|email';
-    //     $this->validate($request, $validateArr, $messageArr);
-
-    //     if (Auth::check()) {
-    //         $id = Auth::user()->id;
-    //     }
-
-    //     $cart = Session::get('cart');
-
-    //     $subtotal = 0;
-    //     foreach ($cart as $key => $value) {
-    //         $subtotal += $value['baseprice'] * $value['qty'];
-    //     }
-    //     // dd($subtotal);
-
-    //     // $subtotal = $request->total_price;
-
-
-
-    //     $order = new orders();
-
-    //     $order->delivery_country = $request->country;
-    //     $order->country_code = $request->country_code;
-    //     $order->delivery_first_name = $request->first_name;
-    //     $order->delivery_last_name = $request->last_name;
-    //     $order->order_company = $request->company_name;
-    //     $order->delivery_address_1 = $request->address_line_1;
-    //     $order->delivery_address_2 = $request->address_line_2;
-    //     $order->delivery_city = $request->city;
-    //     $order->delivery_state = $request->state;
-    //     $order->delivery_zip_code = $request->zip_code;
-    //     $order->area = $request->area;
-    //     $order->landmark = $request->landmark;
-    //     $order->floor_num = $request->floor_num;
-    //     $order->building = $request->building;
-    //     $order->order_shipping = $cart['shipping'];
-    //     $order->country_code = $request->country_code;
-    //     $order->street_number = $request->street_number;
-
-    //     $order->order_email = $request->email;
-    //     $order->delivery_phone_no = $request->phone_no;
-    //     $order->order_notes = $request->order_notes;
-    //     $order->order_company = $request->company_name;
-    //     $order->payment_method = $request->payment_method;
-    //     $order->discount = $request->discount;
-    //     $order->gift = $request->gift;
-
-    //     $order->order_items = count(Session::get('cart'));
-
-    //     $order->order_item_total = $subtotal;
-
-
-    //     $total += $subtotal + $cart['shipping'];
-
-    //     $t_variation_price = $request->total_variation_price ?? 0;
-
-    //     // $t_variation_price = $request->total_variation_price;
-
-    //     $order->order_total = $total;
-
-
-
-    //     $order->user_id = $id;
-
-    //     if (isset($_POST['payment_method']) && $_POST['payment_method'] == 'paypal') {
-    //         $order->transaction_id = $_POST['payment_id'];
-    //         $order->order_status = $_POST['payment_status'];
-    //         $order->card_token = $_POST['payer_id'];
-    //     } elseif (isset($_POST['payment_method']) && $_POST['payment_method'] == 'cash') {
-    //         $order->order_status = "succeeded";
-    //     } else {
-
-    //         try {
-
-    //             try {
-    //                 Stripe\Stripe::setApiKey(config('services.stripe.secret'));
-
-    //                 $customer = \Stripe\Customer::create(array(
-    //                     'email' => $request->email,
-    //                     'name' => $request->first_name,
-    //                     'phone' => $request->phone_no,
-    //                     'description' => "Client Created From Website",
-    //                     'source' => $request->stripeToken,
-    //                 ));
-    //             } catch (Exception $e) {
-    //                 return redirect()->back()->with('stripe_error', $e->getMessage());
-    //             }
-
-    //             try {
-    //                 $charge = \Stripe\Charge::create(array(
-    //                     'customer' => $customer->id,
-    //                     'amount' => $total * 100,
-    //                     'currency' => 'USD',
-    //                     'description' => "Payment From Website",
-    //                     'metadata' => array("name" => $request->first_name, "email" => $request->email),
-    //                 ));
-    //                 // dd($charge);
-    //             } catch (Exception $e) {
-    //                 return redirect()->back()->with('stripe_error', $e->getMessage());
-    //             }
-    //         } catch (Exception $e) {
-    //             return redirect()->back()->with('stripe_error', $e->getMessage());
-    //         }
-    //         $chargeJson = $charge->jsonSerialize();
-    //         // Check whether the charge is successful
-    //         if ($chargeJson['amount_refunded'] == 0 && empty($chargeJson['failure_code']) && $chargeJson['paid'] == 1 && $chargeJson['captured'] == 1) {
-    //             $transactionID = $chargeJson['balance_transaction'];
-    //             $payment_status = $chargeJson['status'];
-    //             $order->transaction_id = $transactionID;
-    //             $order->order_status = $payment_status;
-    //         }
-    //     }
-
-    //     $record = orders::latest()->first();
-    //     $expNum = explode('-', $record->invoice_number);
-    //     $order->invoice_number = rand(0, 999999999999999);
-
-    //     if ($order->save()) {
-
-    //         $orders = orders::orderBy('id', 'desc')
-    //             ->first();
-    //         $subtotal = 0;
-    //         foreach ($cart as $key => $value) {
-    //             if ($value['name'] != '') {
-    //                 $order_products = new orders_products;
-    //                 $order_products->order_products_product_id = $value['id'];
-    //                 $order_products->user_id = Auth::user()->id;
-    //                 $order_products->order_products_name = $value['name'];
-    //                 $order_products->order_products_price = $value['baseprice'];
-    //                 $order_products->orders_id = $orders->id;
-    //                 $order_products->order_products_qty = $value['qty'];
-    //                 $order_products->mat_language = $value['mat_language'];
-    //                 $order_products->shipping = $cart['shipping'];
-    //                 $order_products->order_products_subtotal = $value['baseprice'] * $value['qty'] + $value['variant_price'];
-    //                 $order_products->t_variation_price = $t_variation_price;
-
-    //                 $order_products->variants = json_encode($value['variation']);
-    //                 $order_products->save();
-    //             }
-    //         }
-
-    //         Session::forget('cart');
-
-    //         Session::flash('message', 'Your Order has been placed Successfully');
-    //         Session::flash('alert-class', 'alert-success');
-    //         //echo "data saved";
-    //         //return;
-    //         if (Auth::check()) {
-    //             return redirect('/');
-    //         } else {
-    //             return redirect('/');
-    //         }
-    //     }
-    // }
-
-
-
     public function placeOrder(Request $request)
     {
-        // dd($request->all());
+        dd($request->all());
         $validateArr = [
             'country' => 'required|max:50',
             'first_name' => 'required|max:255',
@@ -607,6 +387,7 @@ class OrderController extends Controller
             }
         }
 
+
         if ($request->has('gift_card_code')) {
             $giftCard = GiftCard::where('code', $request->gift_card_code)->first();
             if ($giftCard && $giftCard->balance > 0) {
@@ -618,7 +399,7 @@ class OrderController extends Controller
         }
 
         // Calculate shipping - prefer request value over session
-        $shippingAmount = $request->shipping_amount ?? ($cart['shipping'] ?? 0);
+        $shippingAmount = $request->shippingRate ?? ($cart['shipping'] ?? 0);
 
         // Create the order
         $order = new orders();
@@ -652,7 +433,6 @@ class OrderController extends Controller
         dd($subtotal, $variationTotal, $shippingAmount, $discountAmount, $giftCardAmount);
         $order->order_total = ($subtotal + $variationTotal + $shippingAmount) - $discountAmount - $giftCardAmount;
         $order->user_id = $id;
-
 
         // Handle payment
         if ($request->payment_method == 'paypal') {
